a6f560af1677e56744c9ee1c2407a02a50d3c653
Sylvester Joosten <sjoosten@anl.gov>
2020-11-07 23:29:11 +0000

    Generalized event generation step

---
+++ dvmp/scripts/print_fname.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+## Simple script to output a unified file name based on a set of data options
+
+EBEAM=
+PBEAM=
+DECAY=
+CONFIG=
+TYPE=
+
+while [ $# -gt 0 ]; do
+  key=$1
+  case $key in
+    --ebeam)
+      EBEAM="$2"
+      shift
+      shift
+      ;;
+    --pbeam)
+      PBEAM="$2"
+      shift
+      shift
+      ;;
+    --decay)
+      DECAY="$2"
+      shift
+      shift
+      ;;
+    --config)
+      CONFIG="$2"
+      shift
+      shift
+      ;;
+    --stage)
+      TYPE="$2"
+      shift
+      shift
+      ;;
+    *)
+      echo "Unknown option $key to print_fname, aborting..."
+      exit 1
+  esac
+done
+
+if [ -z $EBEAM ]; then
+  echo "EBEAM not defined in print_fname, aborting..."
+  exit 1
+elif [ -z $PBEAM ]; then
+  echo "PBEAM not defined in print_fname, aborting..."
+  exit 1
+elif [ -z $DECAY ]; then
+  echo "DECAY not defined in print_fname, aborting..."
+  exit 1
+elif [ -z $CONFIG ]; then
+  echo "CONFIG not defined in print_fname, aborting..."
+elif [ -z $TYPE ]; then
+  echo "TYPE not defined in print_fname, aborting..."
+fi
+
+echo "${CONFIG}_${DECAY}-${EBEAM}on${PBEAM}-${TYPE}"
