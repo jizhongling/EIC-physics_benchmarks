7c2c013ec3fe57be81156c5e6684ad5a48fbf513
Sylvester Joosten <sjoosten@anl.gov>
2020-11-10 19:07:51 +0000

    Added launcher script

---
+++ util/start_dev_shell.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+OS=`uname -s`
+
+if [ "${OS}" = "Linux" ]; then
+  echo "Detected OS: Linux"
+  if [ ! -f juggler_latest.sif ]; then
+    echo "Need to create singularity image"
+    singularity pull docker://docker.io/sly2j/juggler:latest
+  fi
+  echo "Launching dev shell (through singularity)..."
+  singularity exec juggler_latest.sif eic-shell
+elif [ "${OS}" = "Darwin" ]; then
+  echo "Detector OS: MacOS"
+  echo "Syncing docker container"
+  docker pull sly2j/juggler:latest
+  echo "Launching dev shell (through docker)..."
+  docker run -i -t --rm sly2j/juggler:latest eic-shell
+else
+  echo "ERROR: dev shell not available for this OS (${OS})"
+fi
