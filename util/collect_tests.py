82d18cd03f656c0486f37d8e6d393a4b40c12099
Sylvester Joosten <sjoosten@anl.gov>
2020-12-22 19:48:05 +0000

    still working on test collection

--- util/collect_tests.py
+++ util/collect_tests.py
@@ -15,4 +15,8 @@ Internally, we will look for the "tests" keyword in each of these
 files to identify them as benchmark components.
 """
 
+import argparse
+import json
 
+if __name__ == "__main__":
+    pass
