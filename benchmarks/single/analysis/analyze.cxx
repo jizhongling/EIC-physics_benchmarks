f5087fa5495449d7c2c5c0fb780ba191a9130bf1
Wouter Deconinck <wouter.deconinck@umanitoba.ca>
2021-10-05 00:23:09 +0000

    Single simulation test on standard reconstruction

---
+++ benchmarks/single/analysis/analyze.cxx
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+#include <ROOT/RDataFrame.hxx>
+
+#include <eicd/ReconstructedParticleData.h>
+
+int analyze(std::string file)
+{
+  // open dataframe
+  ROOT::RDataFrame df("events", file, {"GeneratedParticles", "ReconstructedParticles"});
+
+  // count total events
+  auto count = df.Count();
+  if (count == 0) {
+    std::cout << "Error: No events found" << std::endl;
+    return -1;
+  }
+
+  auto n_tracks = [](const std::vector<eic::ReconstructedParticleData> &p) { return (int) p.size(); };
+
+  auto d = df
+  .Define("n_tracks_gen", n_tracks, {"GeneratedParticles"})
+  .Define("n_tracks_rec", n_tracks, {"ReconstructedParticles"})
+  ;
+
+  auto stats_n_tracks_gen = d.Stats("n_tracks_gen");
+  auto stats_n_tracks_rec = d.Stats("n_tracks_rec");
+  if (stats_n_tracks_rec->GetMean() < 0.9) {
+    std::cout << "Error: too few tracks per events " << std::endl;
+    stats_n_tracks_gen->Print();
+    stats_n_tracks_rec->Print();
+    return -1;
+  }
+
+  // success
+  return 0;
+}
---
+++ benchmarks/single/single.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ ! -n  "${JUGGLER_N_EVENTS}" ]] ; then 
+  export JUGGLER_N_EVENTS=100
+fi
+
+export JUGGLER_FILE_NAME_TAG="${1:-e-_1GeV_45to135deg}"
+export JUGGLER_GEN_FILE="benchmarks/single/${JUGGLER_FILE_NAME_TAG}.steer"
+export JUGGLER_SIM_FILE="sim_${JUGGLER_FILE_NAME_TAG}.root"
+export JUGGLER_REC_FILE="rec_${JUGGLER_FILE_NAME_TAG}.root"
+
+# Simulate
+npsim --runType run \
+      --printLevel WARNING \
+      --enableGun \
+      --steeringFile ${JUGGLER_GEN_FILE} \
+      --numberOfEvents ${JUGGLER_N_EVENTS} \
+      --part.minimalKineticEnergy 1*TeV  \
+      --compactFile ${DETECTOR_PATH}/${JUGGLER_DETECTOR}.xml \
+      --outputFile  ${JUGGLER_SIM_FILE}
+if [[ "$?" -ne "0" ]] ; then
+  echo "ERROR running npsim"
+  exit 1
+fi
+
+# Reconstruct
+for rec in options/*.py ; do
+  unset tag
+  [[ $(basename ${rec} .py) =~ (.*)\.(.*) ]] && tag=".${BASH_REMATCH[2]}"
+  JUGGLER_REC_FILE=${JUGGLER_REC_FILE/.root/${tag:-}.root} \
+    gaudirun.py ${rec}
+  if [[ "$?" -ne "0" ]] ; then
+    echo "ERROR running juggler"
+    exit 1
+  fi
+done
+
+# Analysis
+root -l -b -q "benchmarks/single/analysis/analyze.cxx(\"${JUGGLER_REC_FILE}\")"
+if [[ "$?" -ne "0" ]] ; then
+  echo "ERROR analysis failed"
+  exit 1
+fi
